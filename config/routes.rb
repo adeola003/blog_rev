c079b0e4b0b946d31d48d7f98271daa746217f58
adeola003 <khadaf3@gmail.com>
2024-05-09 20:53:25 +0100

    create portfolio controller and index action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :blogs
+  get "portfolios", to: 'portfolios#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
