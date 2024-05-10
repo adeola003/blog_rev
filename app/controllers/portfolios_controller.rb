ceed86e41fcad9daecaa3d2fdc1530063cae1c0c
adeola003 <khadaf3@gmail.com>
2024-05-10 16:20:40 +0100

    add action new for portfolios and create the form

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,5 +1,23 @@
 class PortfoliosController < ApplicationController
+
   def index
     @portfolios = Portfolio.all
   end
+
+  def new
+    @portfolio = Portfolio.new
+
+  end
+
+  def create
+
+  end
+
+  private
+
+  def portfolio_params
+    params.require(:portfolio).permit(:title, :subtitle, :body)
+
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :blogs
   get "portfolios", to: 'portfolios#index'
+
+  get "/portfolios/new", to: 'portfolios#new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
