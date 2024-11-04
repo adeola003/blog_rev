551550e40b1a9ea8b2d5eed62546fcf2bf7ce6b9
adeola003 <khadaf3@gmail.com>
2024-11-04 21:29:56 +0100

    add topic reference to blogs

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_11_04_202015) do
+ActiveRecord::Schema[7.0].define(version: 2024_11_04_202813) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,6 +25,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_04_202015) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "status", default: 0
+    t.bigint "topic_id"
+    t.index ["topic_id"], name: "index_blogs_on_topic_id"
   end
 
   create_table "portfolios", force: :cascade do |t|
@@ -50,4 +52,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_11_04_202015) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "blogs", "topics"
 end
