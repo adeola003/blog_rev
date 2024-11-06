aa1fffdf3bf7ae64648993fa0666050f6825ecdd
adeola003 <khadaf3@gmail.com>
2024-11-06 17:35:33 +0100

    add default values to portfolios

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -3,4 +3,11 @@ class Portfolio < ApplicationRecord
   def self.ang
     where(subtitle: "Angular")
   end
+
+  after_initialize :set_defaults
+
+  def set_defaults
+    self.main_image ||= "https://place-hold.it/600x450"
+    self.thumb_image ||= "https://place-hold.it/300x150"
+  end
 end
